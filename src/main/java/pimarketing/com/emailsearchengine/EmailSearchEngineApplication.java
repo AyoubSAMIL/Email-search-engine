e1de845f9fae4e6d61094da59fccf1163a36a63f
ayoubSamil <asamil@pimarketing.com>
2020-12-29 11:28:59 +0100

    Feat: Init project

---
+++ src/main/java/pimarketing/com/emailsearchengine/EmailSearchEngineApplication.java
@@ -0,0 +1,13 @@
+package pimarketing.com.emailsearchengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailSearchEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailSearchEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pimarketing/com/emailsearchengine/EmailSearchEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package pimarketing.com.emailsearchengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailSearchEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
